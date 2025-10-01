88ddec424181fad44284c9efbbd89e20d8257326
whashishkumar <wh.ashishkumar@gmail.com>
2025-10-01 16:01:34 +0530

    image render issue vercel

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source:
-          "/:all*(svg|jpg|jpeg|png|gif|webp|avif|ico|css|js|woff2|ttf|eot)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=0, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -32,7 +8,7 @@ const nextConfig = {
         pathname: "/storage/**",
       },
     ],
-    unoptimized: process.env.NEXT_PUBLIC_DISABLE_IMAGE_OPTIMIZATION === "false",
+    unoptimized: "true",
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
