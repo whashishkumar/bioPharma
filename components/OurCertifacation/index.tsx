2eb4093103d3cf4d0ca32a916d0b7d420b718d34
whashishkumar <wh.ashishkumar@gmail.com>
2025-10-03 18:07:53 +0530

    fix bg overlay

--- components/OurCertifacation/index.tsx
+++ components/OurCertifacation/index.tsx
@@ -66,7 +66,7 @@ export default function OurCertification() {
         </div>
       </div>
       {/* <Image src={"/images/play.png"} alt={"play"} height={800} width={1820} /> */}
-      <div className="relative w-full h-[400px] md:h-[500px] lg:h-[600px] rounded-2xl overflow-hidden bg-gray-100">
+      <div className="relative w-full h-[400px] md:h-[500px] lg:h-[600px] rounded-2xl overflow-hidden">
         {image && (
           <div>
             <video
