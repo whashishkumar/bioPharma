98e580689c1970dd27fcca7330429e7bd0b8fd65
whashishkumar <wh.ashishkumar@gmail.com>
2025-11-07 15:56:01 +0530

    change rotaion size

--- components/OurProducts/index.tsx
+++ components/OurProducts/index.tsx
@@ -32,6 +32,7 @@ export default function OurProducts() {
     setActiveProduct(slug);
     fetchPharmaProducts(slug);
   };
+
   useEffect(() => {
     fetchPharmaProductsMenu();
     fetchPharmaProducts(activeProduct);
