abcc588ebe8567ec4e40ea422ad5762950098fde
whashishkumar <wh.ashishkumar@gmail.com>
2025-12-01 14:44:18 +0530

    fix blogs issue

--- components/BlogDescription/index.tsx
+++ components/BlogDescription/index.tsx
@@ -1,102 +1,105 @@
-"use client";
+/** @format */
 
-import Image from "next/image";
-import { ArrowUpRight } from "lucide-react";
-import { useBlogsContext } from "@/context/OurBlogsContext";
-import { parseBlogContent } from "@/utils/parseContent";
-import Loader from "@/ui/Loader";
-import { useEffect } from "react";
+'use client';
+
+import Image from 'next/image';
+import { ArrowUpRight } from 'lucide-react';
+import { useBlogsContext } from '@/context/OurBlogsContext';
+import { parseBlogContent } from '@/utils/parseContent';
+import Loader from '@/ui/Loader';
+import { useEffect } from 'react';
 
 interface Category {
-  id: number;
-  name: string;
-  slug: string;
-  url: string;
+	id: number;
+	name: string;
+	slug: string;
+	url: string;
 }
 
 interface Tag {
-  id: number;
-  name: string;
-  slug: string;
+	id: number;
+	name: string;
+	slug: string;
 }
 
 interface Blog {
-  id: number;
-  name: string;
-  description: string;
-  content: string;
-  image: string;
-  categories: Category[];
-  tags: Tag[];
-  created_at: string;
+	id: number;
+	name: string;
+	description: string;
+	content: string;
+	image: string;
+	categories: Category[];
+	tags: Tag[];
+	created_at: string;
 }
 
 interface BlogDescriptionProps {
-  blog: Blog;
+	blog: Blog;
 }
 
 export default function BlogDescription({ slug }: any) {
-  const { blogDetail, blogDetailLoading } = useBlogsContext();
-  const { data }: any = blogDetail;
-  const { fetchSingleBlog } = useBlogsContext();
-  const htmlContent = parseBlogContent(data?.content);
+	const { blogDetail, blogDetailLoading } = useBlogsContext();
+	const { data }: any = blogDetail;
+	const { fetchSingleBlog } = useBlogsContext();
+	const htmlContent = parseBlogContent(data?.content);
 
-  useEffect(() => {
-    fetchSingleBlog(slug);
-  }, []);
+	useEffect(() => {
+		fetchSingleBlog(slug);
+	}, []);
 
-  return (
-    <div className="hero-child-container">
-      <div className="py-10">
-        {blogDetailLoading ? (
-          <Loader />
-        ) : (
-          <div className="px-4">
-            <div className="relative h-80 w-full rounded-2xl overflow-hidden mb-6">
-              {data?.image && (
-                <Image
-                  src={data?.image.replace("Image preview", "")}
-                  alt={"blog.name"}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-2xl"
-                />
-              )}
-            </div>
-            <h1 className="text-3xl md:text-4xl font-bold text-[#172C45] mb-4 poppins">
-              {data?.name}
-            </h1>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {data?.categories.map((cat: any) => (
-                <a
-                  key={cat.id}
-                  href={cat.url}
-                  className="bg-green-100 text-green-800 text-xs px-3 py-1 rounded-full hover:bg-green-200 transition"
-                >
-                  {cat.name}
-                </a>
-              ))}
-            </div>
-            <div className="flex flex-wrap gap-2 mb-6">
-              {data?.tags.map((tag: any) => (
-                <span
-                  key={tag.id}
-                  className="bg-gray-100 text-gray-700 text-xs px-2 py-1 rounded-full "
-                >
-                  {tag.name}
-                </span>
-              ))}
-            </div>
-            <p className="text-gray-700 text-lg mb-6">{data?.description}</p>
-            <div
-              className="prose text-gray-700 text-lg mb-6"
-              dangerouslySetInnerHTML={{
-                __html: htmlContent,
-              }}
-            />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	console.log(data?.content, '1234567890');
+	return (
+		<div className='hero-child-container'>
+			<div className='py-10'>
+				{blogDetailLoading ? (
+					<Loader />
+				) : (
+					<div className='px-4'>
+						<div className='relative h-80 w-full rounded-2xl overflow-hidden mb-6'>
+							{data?.image && (
+								<Image
+									src={data?.image.replace('Image preview', '')}
+									alt={'blog.name'}
+									layout='fill'
+									objectFit='cover'
+									className='rounded-2xl'
+								/>
+							)}
+						</div>
+						<h1 className='text-3xl md:text-4xl font-bold text-[#172C45] mb-4 poppins'>
+							{data?.name}
+						</h1>
+						<div className='flex flex-wrap gap-2 mb-4'>
+							{data?.categories.map((cat: any) => (
+								<a
+									key={cat.id}
+									href={cat.url}
+									className='bg-green-100 text-green-800 text-xs px-3 py-1 rounded-full hover:bg-green-200 transition'
+								>
+									{cat.name}
+								</a>
+							))}
+						</div>
+						<div className='flex flex-wrap gap-2 mb-6'>
+							{data?.tags.map((tag: any) => (
+								<span
+									key={tag.id}
+									className='bg-gray-100 text-gray-700 text-xs px-2 py-1 rounded-full '
+								>
+									{tag.name}
+								</span>
+							))}
+						</div>
+						<p className='text-gray-700 text-lg mb-6'>{data?.description}</p>
+						<div
+							className='prose text-gray-700 text-lg mb-6'
+							dangerouslySetInnerHTML={{
+								__html: htmlContent,
+							}}
+						/>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
--- utils/parseContent.ts
+++ utils/parseContent.ts
@@ -1,28 +1,30 @@
+/** @format */
+
 // utils/parseBlogContent.ts
 export const parseBlogContent = (content: string) => {
-  // Replace [youtube-video]URL[/youtube-video] with iframe HTML
-  return content?.replace(
-    /\[youtube-video\](.*?)\[\/youtube-video\]/g,
-    (_, url) => {
-      try {
-        const videoId = new URL(url).searchParams.get("v");
-        if (videoId) {
-          return `<div class="my-6 w-full h-[400px]">
-            <iframe
-              width="100%"
-              height="100%"
-              src="https://www.youtube.com/embed/${videoId}"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>`;
-        }
-        return "";
-      } catch {
-        return "";
-      }
-    }
-  );
+	// Replace [youtube-video]URL[/youtube-video] with iframe HTML
+	return content?.replace(
+		/\[youtube-video\](.*?)\[\/youtube-video\]/g,
+		(_, url) => {
+			try {
+				const videoId = new URL(url).searchParams.get('v');
+				// if (videoId) {
+				// 	return `<div class="my-6 w-full h-[400px]">
+				//     <iframe
+				//       width="100%"
+				//       height="100%"
+				//       src="https://www.youtube.com/embed/${videoId}"
+				//       title="YouTube video player"
+				//       frameborder="0"
+				//       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				//       allowfullscreen
+				//     ></iframe>
+				//   </div>`;
+				// }
+				return '';
+			} catch {
+				return '';
+			}
+		},
+	);
 };
